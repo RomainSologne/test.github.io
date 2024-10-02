b3448248dfca0f9bbd89a77da171f47f7c37ecb7
RomainSologne <romaint.sologne@gmail.com>
2024-10-02 11:03:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,51 @@
 // Initialiser la carte Leaflet
-var map = L.map('map').setView([48.8566, 2.3522], 6); // Coordonnées centrées sur la France
+var map = L.map('map').setView([48.8566, 2.3522], 12); // Coordonnées centrées sur Paris
 
-// Ajouter la couche de base OpenStreetMap
+// Ajouter une couche de tuiles OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Fonction pour charger le GeoJSON et ajouter une couche 3D avec Deck.gl
-function add3DLayer(data) {
-    const deckLayer = new deck.GeoJsonLayer({
-        id: 'geojson-layer',
-        data: Paris, // Les données GeoJSON
-        extruded: true, // Active l'extrusion 3D
-        filled: true,
-        wireframe: true,
-        getFillColor: [255, 140, 0, 160], // Couleur de remplissage
-        getLineColor: [255, 255, 255], // Couleur des lignes de contour
-        getElevation: d => d.properties.Ind * 10, // Extrusion basée sur la densité de population
-        getRadius: 100,
-        pointRadiusMinPixels: 5,
-        pointRadiusScale: 2000,
-    });
+// Charger les données GeoJSON à partir du fichier kepler.gl JSON
+const geoJsonData = {
+  "type": "FeatureCollection",
+  "features": [
+    // Vous pouvez copier les features extraites de votre fichier ici
+    // Exemple d'un feature simplifié
+    {
+      "type": "Feature",
+      "properties": {
+        "Groupe": 351367,
+        "Ind": 3
+      },
+      "geometry": {
+        "type": "MultiPolygon",
+        "coordinates": [
+          [[[2.45204, 48.81567], [2.45177, 48.81746], [2.45448, 48.81764], [2.45475, 48.81586], [2.45204, 48.81567]]]
+        ]
+      }
+    }
+  ]
+};
 
-    // Ajouter la couche Deck.gl à Leaflet
-    L.deckGL({
-        layers: [deckLayer],
-        map
-    }).addTo(map);
+// Fonction pour créer une couche Deck.gl 3D
+function add3DLayer(data) {
+  const deckLayer = new deck.GeoJsonLayer({
+    id: 'geojson-layer',
+    data: data,
+    extruded: true,
+    filled: true,
+    wireframe: true,
+    getFillColor: [255, 140, 0, 160], // Couleur de remplissage
+    getElevation: d => d.properties.Ind * 100, // Extrusion basée sur la propriété "Ind"
+    getLineColor: [255, 255, 255]
+  });
+
+  L.deckGL({
+    layers: [deckLayer],
+    map
+  }).addTo(map);
 }
 
-// Charger le fichier GeoJSON contenant les données INSEE
-fetch('Paris.geojson')
-    .then(response => response.json())
-    .then(data => add3DLayer(data))
-    .catch(error => console.error('Erreur lors du chargement des données:', error));
+// Ajouter les données GeoJSON à la carte
+add3DLayer(geoJsonData);
